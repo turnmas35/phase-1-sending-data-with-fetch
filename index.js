a90b1a2d0b71ba72e02f58b9d8d75df5854927e0
Matthew Turner <turnmas35@gmail.com>
2023-11-20 19:40:45 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+function submitData(name, email) {
+    const data = {
+        name: name,
+        email: email
+      };
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify(data)
+      })
+      .then(res => res.json())
+      .then(json => appendIdToDOM(json))
+      .catch(error => document.body.innerHTML = error.message)
+    };
+function appendIdToDOM(json){
+    document.body.innerHTML = json.id
+}
